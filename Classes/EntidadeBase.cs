a249a791e62647a44e7c4dc10c2b37d3ea5782be
vanessa_oliveira <vanessa.oliveira.80@gmail.com>
2021-07-01 01:15:36 -0300

    console app cadastro de series

---
+++ Classes/EntidadeBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dio_cadastro_serie.Classes
+{
+    public abstract class EntidadeBase
+    {
+        public int Id { get; protected set; }
+    }
+}
